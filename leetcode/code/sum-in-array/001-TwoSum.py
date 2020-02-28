5515cd622df564f2c8bd42f8a5ab75bd9766478d
dyq666 <694526892@qq.com>
2020-02-28 22:12:52 +0800

    feature: remove old solutions of LeetCode 1

--- leetcode/code/sum-in-array/001-TwoSum.py
+++ leetcode/code/sum-in-array/001-TwoSum.py
@@ -1,20 +1,4 @@
-# _*_ coding: utf-8 _*_
-__author__ = "dyq666"
-__date__ = "2018/5/25 20:02"
-
-
 class Solution:
-    def twoSum(self, nums, target):
-        """
-        1. O(n^2)
-        2. 暴力循环
-        """
-        nums_len = len(nums)
-
-        for i in range(0, nums_len):
-            for j in range(i + 1, nums_len):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
 
     def twoSum02(self, nums, target):
         """
@@ -44,24 +28,3 @@ class Solution:
                 # 如果值相同就查找下一个该值的索引
                 right_index = raw_nums.index(v_right, left + 1) if v_right == v_left else raw_nums.index(v_right)
                 return [left_index, right_index]
-
-    def twoSum03(self, nums, target):
-        """
-        1. O(n)
-        2. 使用Hash
-        """
-        seen_dict = {}
-
-        for i, num in enumerate(nums):
-
-            search = target - num
-            if search in seen_dict:
-                return [seen_dict[search], i]
-            else:
-                seen_dict[num] = i
-
-
-if __name__ == '__main__':
-    solution = Solution()
-    # print(solution.twoSum02([2, 7, 11, 15], 9))
-    print(solution.twoSum02([2, 3, 3, 15], 6))
