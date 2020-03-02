f285f2423d8103026aa36528c1c35140c12cad5c
dyq666 <694526892@qq.com>
2020-03-02 20:43:04 +0800

    feature: (C) finish LeetCode 1 with colliding pointer

--- leetcode/1.c
+++ leetcode/1.c
@@ -4,6 +4,8 @@
 
 #include <stdlib.h>
 
+int _compare_int(const void *a, const void *b);
+
 int* twoSum(const int* nums, int numsSize, int target, int* returnSize) {
     /* 核心方法 - Combination. */
     const int SIZE = 2;
@@ -25,4 +27,58 @@ int* twoSum(const int* nums, int numsSize, int target, int* returnSize) {
     }
 
     return res;
+}
+
+int* twoSum2(const int* nums, int numsSize, int target, int* returnSize) {
+    /* 核心方法 - 对撞指针. */
+    const int SIZE = 2;
+    int sum, new_nums[numsSize];
+    int l = 0, r = numsSize - 1, *res = malloc(SIZE * sizeof(int));
+
+    // 不修改原有的 `nums`.
+    for (int i = 0; i < numsSize; i ++) {
+        new_nums[i] = nums[i];
+    }
+
+    // TODO 这里需要想办法让 `nums` 的索引参与排序中, 可能需要 struct.
+    qsort(new_nums, numsSize, sizeof(int), _compare_int);
+
+    while (l < r) {
+        sum = new_nums[l] + new_nums[r];
+        if (sum == target) {
+            // 初始化 res, -1 作为默认值, 用于判断当前位置是否被修改过.
+            for (int i = 0; i < SIZE; i ++) {
+                res[i] = -1;
+            }
+            // res 的每个位置只能被修改一次, 用找到的值去 `nums` 中找到原始的索引.
+            for (int i = 0; i < numsSize; i ++) {
+                if (res[0] == -1 && nums[i] == new_nums[l]) {
+                    res[0] = i;
+                } else if (res[1] == - 1 && nums[i] == new_nums[r]) {
+                    res[1] = i;
+                }
+            }
+            *returnSize = SIZE;
+            return res;
+        } else if (sum < target) {
+            l ++;
+        } else {
+            r --;
+        }
+    }
+
+    return res;
+}
+
+int _compare_int(const void *a, const void *b) {
+    /* copy from https://devdocs.io/c/algorithm/qsort */
+    int value_a = *((const int *) a);
+    int value_b = *((const int *) b);
+
+    if (value_a < value_b) {
+        return -1;
+    } else if (value_a > value_b) {
+        return 1;
+    }
+    return 0;
 }
