b92879b649d8f972a79622cf3bd7be73ddfe0557
dyq666 <694526892@qq.com>
2020-03-08 19:55:28 +0800

    opt: (C) finish todos in LeetCode 3

--- leetcode/3.c
+++ leetcode/3.c
@@ -1,18 +1,18 @@
-/* TODO LIST
- 1. while (*r) 是否等于 while (*r != EOF), 以及是否应该显式的使用 EOF.
- 2. 如何初始化数组使所有元素为 0.
- 3. chars 代替哈希.
- */
-
 #include <iso646.h>
 
 int lengthOfLongestSubstring(char *s) {
+    /* 核心方法 - Hash.
+
+    这里默认了 `s` 中只有 0-127 的 ASCII code, 因此可以使用 128 长度的
+    整数数组完成哈希表的功能, 当字符对应的值为 0 时代表当前字符不在哈希表中,
+    反之, 当为 1 时字符在哈希表.
+    */
     int chars[128] = {0};
     int max_len = 0;
     char *l = s, *r = s;
 
-    while (*l) {
-        if (*r and chars[*r] == 0) {
+    while (*l != '\0') {
+        if (*r != '\0' and chars[*r] == 0) {
             chars[*r] = 1;
             r ++;
             max_len = max_len > r - l ? max_len : r - l;
