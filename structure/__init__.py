9e80a77198b452b3c81ab01db70c13f963a86fc1
dyq666 <694526892@qq.com>
2020-03-12 09:19:07 +0800

    feature: (Python) add `DynamicArray`

---
+++ structure/__init__.py
@@ -0,0 +1,84 @@
+from typing import Any, Iterable, Optional
+
+
+class DynamicArray:
+    """动态数组.
+
+    动态数组是在静态数组的基础上封装的. 在 Python 中, 没有常用的静态数组,
+    在这里我们限制下 list 的功能, 让它扮演静态数组的角色 (list 实际上就是动态数组).
+
+    动态数组的核心有两个:
+      1. capacity (容量) 和 size. capacity 代表底层静态数组的大小, 而 size 代表动态数组的大小.
+         二者满足 size <= capacity 且 capacity != 0 (在本类中限制 capacity >= 10).
+      2. 扩容和缩容. 增加元素时如果 size == capacity (相当于静态数组满了), 就需要增大底层的静态数组.
+         删除元素时如果 size == capacity / 4 (这里具体到容量的多少才缩容, 可以按需更改),
+         就需要缩减底层的静态数组, 通常让缩减后的 capacity 仍大于 size. 例如在本类中,
+         当 size == capacity / 4 时缩容, 将容积缩为 capactiy / 2, 此时 capacity == 2 * size,
+         动态数组中仍有空间可以添加元素. 如果容积缩为 capacity / 4, 此时 capacity == size,
+         添加元素就又需要扩容了, 消耗时间.
+
+    TODO 还需要增加 __getitem__ __setitem__
+    """
+
+    MIN_SIZE = 10
+
+    def __init__(self):
+        # 默认 capacity 为 10.
+        self._data = [None for _ in range(self.MIN_SIZE)]
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def __repr__(self) -> str:
+        return repr(self._data)
+
+    def __iter__(self) -> Iterable:
+        return iter(self._data[:self._size])
+
+    def __contains__(self, item: Any) -> bool:
+        return item in self._data[:self._size]
+
+    def insert(self, value: Any, index: Optional[int] = None):
+        """插入元素.
+
+        如果不传 `index` 则向末尾插入.
+        """
+        index = self._size if index is None else index
+
+        if not (0 <= index <= self._size):
+            raise IndexError
+
+        if self._size == len(self._data):
+            self._resize(len(self._data) * 2)
+
+        self._data[index + 1: self._size + 1] = self._data[index: self._size]
+        self._data[index] = value
+        self._size += 1
+
+    def pop(self, index: Optional[int] = None) -> Any:
+        """删除元素.
+
+        如果不传 `index` 则从末尾删除.
+        """
+        index = self._size - 1 if index is None else index
+
+        if not (0 <= index < self._size):
+            raise IndexError
+
+        if self._size == len(self._data) // 4:
+            self._resize(len(self._data) // 2)
+
+        res = self._data[index]
+        self._data[index: self._size - 1] = self._data[index + 1: self._size]
+        self._size -= 1
+        return res
+
+    def _resize(self, capacity: int):
+        """改变静态数组的容量."""
+        # 永久使 capacity >= 10
+        if capacity < self.MIN_SIZE:
+            return
+        old = self._data
+        self._data = [None for _ in range(capacity)]
+        self._data[:self._size] = old[:self._size]
---
+++ test_structure.py
@@ -0,0 +1,102 @@
+import pytest
+
+from structure import DynamicArray
+
+
+class TestDynamicArray:
+
+    def test_normal(self):
+        d = DynamicArray()
+        # 默认情况下容量是 10, 大小是 0
+        assert len(d._data) == 10
+        assert len(d) == 0
+
+    def test_insert(self):
+        d = DynamicArray()
+
+        # 不能在小于 0 或大于 size 的位置插入元素
+        with pytest.raises(IndexError):
+            d.insert('a', -1)
+        with pytest.raises(IndexError):
+            d.insert('a', len(d) + 1)
+
+        # 从末尾插入 10 个元素
+        for i in range(1, 11):
+            d.insert('c')
+            assert len(d._data) == 10
+            assert len(d) == i
+        # 插入结束后 size == capactiy
+        assert len(d) == len(d._data)
+
+        # 再插入 10 个元素, 此时容量变成之前的两倍
+        for i in range(11, 21):
+            d.insert('zzz')
+            assert len(d._data) == 20
+            assert len(d) == i
+
+    def test_pop(self):
+        d = DynamicArray()
+        for i in range(20):
+            d.insert(i)
+
+        # 不能在小于 0 或大于 size - 1 的位置茶元素
+        with pytest.raises(IndexError):
+            d.pop(-1)
+        with pytest.raises(IndexError):
+            d.pop(len(d))
+
+        # 从末尾删除元素
+        for i in range(15):
+            assert 19 - i == d.pop()
+            assert len(d._data) == 20
+            assert len(d) == 19 - i
+
+        # 缩容, 20 // 4 == 5
+        assert len(d) == 5
+        assert len(d._data) == 20
+        d.pop()
+        assert len(d) == 4
+        assert len(d._data) == 10
+
+        # 由于限制了容量必须 >= 10, 所以当 10 // 4 = 2 时, 也没有缩容.
+        for i in range(4):
+            d.pop()
+            assert len(d) == 3 - i
+            assert len(d._data) == 10
+
+        # 空数组不能 pop
+        with pytest.raises(IndexError):
+            d.pop()
+
+    def test_contains(self):
+        d = DynamicArray()
+        for i in range(9):
+            d.insert(i)
+
+        # None 在底层的静态数组中, 但不在动态数组中
+        assert None not in d
+        assert None in d._data
+
+        for i in range(9):
+            assert i in d
+
+    def test_iter(self):
+        d = DynamicArray()
+
+        # 空数组
+        assert list(iter(d)) == []
+
+        # 插满元素
+        for i in range(10):
+            d.insert(i)
+        assert list(iter(d)) == list(range(10))
+
+        # 扩容
+        d.insert(100, 1)
+        assert list(iter(d)) == [0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        assert d._data == [0, 100, 1, 2, 3, 4, 5, 6, 7, 8,
+                           9, None, None, None, None, None, None, None, None, None]
+
+        # 删除
+        d.pop(2)
+        assert list(iter(d)) == [0, 100, 2, 3, 4, 5, 6, 7, 8, 9]
