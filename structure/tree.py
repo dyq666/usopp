d093a3cc865739e412173b2a20bae583a98a0b8b
dyq666 <694526892@qq.com>
2020-03-16 18:16:37 +0800

    feature: add preorder with mocked func calling stack

--- structure/tree.py
+++ structure/tree.py
@@ -28,15 +28,15 @@ class BinaryTree:
         如何看待迭代实现和递归实现二者的异同 ?
 
         答: 实际上二者逻辑类似. 假设每条语句存储到栈中依次执行, 那么最后一条语句
-        一定在栈底, 再将递归实现的语句缩成最后两句, 我们可以得到以下示意结果. 初始栈
-        中有 preorder(左节点), preored(右节点) 两条语句, 当执行到 preorder(左节点)
-        时, 需要将此函数的语句 (preorder(左节点.左节点) preorder(左节点.右节点)) 入栈.
-        因此迭代的实现其实是在模拟一个系统栈运行函数.
-
+        一定在栈底, `_preorder` 中模拟系统栈实现了前序遍历. 下图展示了 `_preorder`
+        运行 `yield root` 和 `preorder root.l` 之后的系统栈. 而对于 `preorder`
+        来说没有记录具体的操作, 因而不能准确的对应到递归实现中的语句, 只不过思路类似,
+        依然是先入右节点再入左节点.
         ```
-        yf preorder(root.l)  -> yf preorder(root.l.l)
-        yf preorder(root.r)     yf preorder(root.l.r)
-                                yf preorder(root.r)
+        yield root           preorder root.l      yield root.l
+        preorder root.l  ->  preorder root.r  ->  preorder root.l.l
+        preorder root.r                           preorder root.l.r
+                                                  preorder root.r
         ```
         """
         if not root:
@@ -46,6 +46,32 @@ class BinaryTree:
             node = nodes.pop()
             yield node
             nodes.extend(n for n in (node.right, node.left) if n)
+
+    @classmethod
+    def _preorder(cls, root: Optional[BinaryTreeNode]
+                  ) -> Optional[Iterable[BinaryTreeNode]]:
+        """模拟系统栈实现前序遍历.
+
+        实际上这种实现可以拓展到中序, 后序等其他实现. 只不过是需要按照
+        递归实现中的语句顺序改变 `_operators` 中不同操作的位置.
+        """
+        def _operators(n: Optional[BinaryTreeNode]) -> list:
+            # 空 list 代表没有任何语句.
+            if n is None:
+                return []
+            return [('preorder', n.right),
+                    ('preorder', n.left),
+                    ('yield', n)]
+
+        if not root:
+            return
+        nodes = _operators(root)
+        while nodes:
+            operator, node = nodes.pop()
+            if operator == 'yield':
+                yield node
+            else:
+                nodes.extend(_operators(node))
 
 
 class BinaryTreeRecursion:
