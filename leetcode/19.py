638af01ac8d0b3a23c59e24a0a5a9d4d726d5ea7
dyq666 <694526892@qq.com>
2020-03-16 08:18:08 +0800

    feature: resolve LeetCode 19 with two points

--- leetcode/19.py
+++ leetcode/19.py
@@ -2,11 +2,10 @@ from .util import ListNode
 
 
 class Solution:
-    """核心方法 - 单指针链表.
+    """核心方法 - 链表.
 
-    遍历两次, 第一次了解链表长度, 第二次删除元素.
-
-    TODO (完成双指针链表.)
+    `removeNthFromEnd` 单指针遍历两次, 第一次了解链表长度, 第二次删除元素.
+    `removeNthFromEnd2` 双指针遍历一次, 两个指针相差 `n` 的距离`.
     """
 
     @staticmethod
@@ -20,6 +19,25 @@ class Solution:
         for _ in range(index):
             needle = needle.next
         needle.next = needle.next.next
+        return dummy.next
+
+    @staticmethod
+    def removeNthFromEnd2(head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(None, head)
+
+        # fast 和被删除的节点之间的距离相差 n, 但由于删除节点需要找到
+        # 前一个元素, 因此让 slow 指向 dummy.
+        fast, slow = head, dummy
+        step = 0
+
+        while fast is not None:
+            if step >= n:
+                slow = slow.next
+            fast = fast.next
+            step += 1
+
+        # 题目中保证了 `n` 是一个有效的值, 即 n ∈ [1, len + 1), 所以一定能删除某个节点.
+        slow.next = slow.next.next
         return dummy.next
 
 
@@ -49,7 +67,8 @@ if __name__ == '__main__':
     2. 删除最后一个元素.
     3. 删除任意一个中间元素.
     """
-    fs = [Solution.removeNthFromEnd, Solution2().removeNthFromEnd]
+    fs = [Solution.removeNthFromEnd, Solution.removeNthFromEnd2,
+          Solution2().removeNthFromEnd]
 
     for f in fs:
         # 1
