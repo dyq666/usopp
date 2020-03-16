883449f1c357b41909d30ddb87f18e8c6bb7b319
dyq666 <694526892@qq.com>
2020-03-16 08:20:10 +0800

    opt: only use one `target_index`

--- leetcode/19.py
+++ leetcode/19.py
@@ -47,17 +47,16 @@ class Solution2:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         # 题目中保证了 `n` 是一个有效的值, 即 n ∈ [1, len + 1).
         index = len(head) - n
-        return self._remove(head, 0, index)
+        return self._remove(head, index)
 
-    def _remove(self, node: ListNode, cur_index: int,
-                target_index: int) -> ListNode:
+    def _remove(self, node: ListNode, target_index: int) -> ListNode:
         """删除节点索引等于目标索引的节点."""
-        # 最小情况, 当前节点等于目标节点, 由于题目保证了 `target_index`
-        # 是有效的, 所以这个条件肯定先于 `node is None`.
-        if cur_index == target_index:
+        # 最小情况, 目标索引为 0, 由于题目保证了 `target_index` 是有效的,
+        # 所以这个条件肯定先于 `node is None`.
+        if target_index == 0:
             return node.next
 
-        node.next = self._remove(node.next, cur_index + 1, target_index)
+        node.next = self._remove(node.next, target_index - 1)
         return node
 
 
