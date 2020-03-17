ecd2fa9d443e2f8a03bcb7eae589a863c22ce991
dyq666 <694526892@qq.com>
2020-03-17 12:12:44 +0800

    feature: add test preorder

--- test/test_tree.py
+++ test/test_tree.py
@@ -1,30 +1,39 @@
 import pytest
 
-from structure import BinaryTreeNode, BinaryTree
+from structure import BinaryTreeNode, BinaryTree, BinaryTreeRecursion
 
 
 @pytest.fixture
 def trees():
-    """返回如下的五棵树.
+    """返回如下的五棵树 (最大层数: 3).
     ```
         1         1      N      1        1
       2   3     2   N         N   9    2   3
-     N 5 N N   9 N N N       N N 8 N
+     N 5 N N   9 N               8 N
     ```
     """
     return [
-        BinaryTreeNode.from_list([1, 2, 3, None, 5, None, None]),
-        BinaryTreeNode.from_list([1, 2, None, 9, None, None, None]),
+        BinaryTreeNode.from_list([1, 2, 3, None, 5]),
+        BinaryTreeNode.from_list([1, 2, None, 9, None]),
         BinaryTreeNode.from_list([None]),
         BinaryTreeNode.from_list([1, None, 9, None, None, 8, None]),
         BinaryTreeNode.from_list([1, 2, 3]),
     ]
 
 
+def test_preorder(trees):
+    fs = [BinaryTree.preorder, BinaryTree._preorder,
+          BinaryTreeRecursion.preorder]
+    for f in fs:
+        assert list(n.val for n in f(trees[0])) == [1, 2, 5, 3]
+        assert list(n.val for n in f(trees[1])) == [1, 2, 9]
+        assert list(n.val for n in f(trees[2])) == []
+        assert list(n.val for n in f(trees[3])) == [1, 9, 8]
+        assert list(n.val for n in f(trees[4])) == [1, 2, 3]
+
+
 def test_inorder(trees):
-
     fs = [BinaryTree.inorder, BinaryTree._inorder]
-
     for f in fs:
         assert list(n.val for n in f(trees[0])) == [2, 5, 1, 3]
         assert list(n.val for n in f(trees[1])) == [9, 2, 1]
