bf7c1b30d260206558dc2ffd36b485025adbe6c6
dyq666 <694526892@qq.com>
2020-03-18 19:59:10 +0800

    opt: adjust test cases of tree

--- test/test_tree.py
+++ test/test_tree.py
@@ -8,7 +8,7 @@ from util import no_value
 
 @pytest.fixture
 def trees():
-    """返回如下的五棵树 (最大层数: 3).
+    """返回如下的一些树 (最大层数: 3).
     ```
         1         1      N      1        1       9
       2   3     2   N         N   9    2   3   8   7
@@ -56,6 +56,7 @@ def test_inorder(trees, f):
     assert list(n.val for n in f(trees[2])) == []
     assert list(n.val for n in f(trees[3])) == [1, 8, 9]
     assert list(n.val for n in f(trees[4])) == [2, 1, 3]
+    assert list(n.val for n in f(trees[5])) == [8, 4, 9, 3, 7]
 
 
 @pytest.mark.parametrize('f', (BTUtil.postorder,))
@@ -65,6 +66,7 @@ def test_postorder(trees, f):
     assert list(n.val for n in f(trees[2])) == []
     assert list(n.val for n in f(trees[3])) == [8, 9, 1]
     assert list(n.val for n in f(trees[4])) == [2, 3, 1]
+    assert list(n.val for n in f(trees[5])) == [4, 8, 3, 7, 9]
 
 
 @pytest.mark.parametrize('f', (BTUtil.levelorder,))
@@ -76,9 +78,12 @@ def test_levelorder(trees, f):
     assert list(n.val for level in f(trees[4]) for n in level) == [1, 2, 3]
     assert list(n.val for level in f(trees[5]) for n in level) == [9, 8, 7, 4, 3]
 
-    assert list(n and n.val for level in f(trees[0], False) for n in level) == [1, 2, 3, None, 5]
-    assert list(n and n.val for level in f(trees[1], False) for n in level) == [1, 2, None, 9, None]
-    assert list(n and n.val for level in f(trees[2], False) for n in level) == []
-    assert list(n and n.val for level in f(trees[3], False) for n in level) == [1, None, 9, 8, None]
-    assert list(n and n.val for level in f(trees[4], False) for n in level) == [1, 2, 3]
-    assert list(n and n.val for level in f(trees[5], False) for n in level) == [9, 8, 7, None, 4, 3, None]
+
+@pytest.mark.parametrize('f', (partial(BTUtil.levelorder, skip_none=False),))
+def test_levelorder(trees, f):
+    assert list(n and n.val for level in f(trees[0]) for n in level) == [1, 2, 3, None, 5]
+    assert list(n and n.val for level in f(trees[1]) for n in level) == [1, 2, None, 9, None]
+    assert list(n and n.val for level in f(trees[2]) for n in level) == []
+    assert list(n and n.val for level in f(trees[3]) for n in level) == [1, None, 9, 8, None]
+    assert list(n and n.val for level in f(trees[4]) for n in level) == [1, 2, 3]
+    assert list(n and n.val for level in f(trees[5]) for n in level) == [9, 8, 7, None, 4, 3, None]
