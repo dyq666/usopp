5284847b2e047414a270b8311eabe26ecd15e51a
dyq666 <694526892@qq.com>
2020-03-19 22:08:10 +0800

    feature: remove old LeetCode 100

--- leetcode/big/100 -- Same Tree.py
+++ leetcode/big/100 -- Same Tree.py
@@ -1,7 +1,3 @@
-from itertools import zip_longest
-
-from libs.tree import BT
-
 class Solution:
     """
     Date: 2018-10-04
@@ -19,18 +15,3 @@ class Solution:
 
         return p.val == q.val and self.isSameTree(p.left, q.left) \
                                 and self.isSameTree(p.right, q.right)
-
-class Solution02:
-    """
-    Date: 2018-10-10
-    Update: 2018-10-21
-    Method: 迭代
-    Solution: 同时 dfs(含 None) 两棵树.
-    """
-    def isSameTree(self, p, q):
-        for n1, n2 in zip_longest(BT.iterdfs(p, False), BT.iterdfs(q, False)):
-            v1 = n1 and n1.val
-            v2 = n2 and n2.val
-            if v1 != v2:
-                return False
-        return True
