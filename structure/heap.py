01579095339cdeb51680a70a9439e95d8c908b17
dyq666 <694526892@qq.com>
2020-03-22 19:36:36 +0800

    feature: add doc of relationship of indexes

--- structure/heap.py
+++ structure/heap.py
@@ -6,12 +6,7 @@ from typing import Tuple
 
 
 class MaxHeap:
-    """最大堆.
-
-    如何推到出父节点和孩子节点之间索引的关系 ?
-
-    答: TODO 完成此问题
-    """
+    """最大堆."""
 
     def __init__(self):
         self._size = 0
@@ -22,10 +17,35 @@ class MaxHeap:
 
     @staticmethod
     def child_idxes(index: int) -> Tuple[int, int]:
+        """获取节点的孩子索引.
+
+        如何推到出父节点和孩子节点之间索引的关系 ?
+
+        答: 根据下图中树和数组的关系, 可以推出索引之间的转换公式.
+        ```
+             0          [0,
+           1   2    ->   1, 2
+          3 4 5 6        3, 4, 5, 6]
+        ```
+
+        0 -> 1, 2
+        1 -> 3, 4
+        2 -> 5, 6
+        ...
+        n -> 2 * n + 1, 2 * n + 2
+
+        1 -> 0
+        2 -> 0
+        3 -> 1
+        4 -> 1
+        ...
+        n -> (n - 1) // 2
+        """
         return index * 2 + 1, index * 2 + 2
 
     @staticmethod
     def parent_idx(index: int) -> int:
+        """获取节点的父索引."""
         if index == 0:
             raise ValueError
         return (index - 1) // 2
