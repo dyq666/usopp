5d3ffe7d1d9685586fc22aadbc8988cb76358f11
dyq666 <694526892@qq.com>
2020-03-25 19:46:25 +0800

    opt: remove duplicate comments

--- structure/segment_tree.py
+++ structure/segment_tree.py
@@ -10,6 +10,7 @@ from .util import check_index
 
 
 class SegmentTree:
+    """线段树 (数组存储)."""
 
     def __init__(self, array: list, tree: list, key: callable):
         self._a = array
@@ -61,12 +62,8 @@ class SegmentTree:
         # 后序遍历, 左节点代表左区间, 右节点代表右区间
         self._build(l, mid, root=BTUtil.left_idx(root))
         self._build(mid + 1, r, root=BTUtil.right_idx(root))
-        self._t[root] = self._merge(root)
-
-    def _merge(self, root: int) -> Any:
-        """融合孩子节点得到父节点的值."""
-        return self.key(self._t[BTUtil.left_idx(root)],
-                        self._t[BTUtil.right_idx(root)])
+        self._t[root] = self.key(self._t[BTUtil.left_idx(root)],
+                                 self._t[BTUtil.right_idx(root)])
 
     def _query(self, l: int, r: int, root: int, query_l: int, query_r: int) -> Any:
         """在以 `root` 为根的线段树中查询区间 [query_l...query_r], 根代表的区间为 [l...r]."""
@@ -84,6 +81,7 @@ class SegmentTree:
             return self._query(mid + 1, r, root=BTUtil.right_idx(root),
                                query_l=query_l, query_r=query_r)
         # 查询区间横跨两个区间, 那么把查询区间分为 [query_l...mid], [mid+1...query_r]
+        # 查询左区间去左区间查, 查询右区间去右查.
         vl = self._query(l, mid, root=BTUtil.left_idx(root),
                          query_l=query_l, query_r=mid)
         vr = self._query(mid + 1, r, root=BTUtil.right_idx(root),
@@ -101,13 +99,18 @@ class SegmentTree:
 
         # `target_idx` 只可能在左区间或右区间
         if target_idx <= mid:
-            self._update(l, mid, target_idx, root=BTUtil.left_idx(root))
+            self._update(l, mid, target_idx=target_idx, root=BTUtil.left_idx(root))
         else:
-            self._update(l, mid, target_idx, root=BTUtil.right_idx(root))
-        self._t[root] = self._merge(root)
+            self._update(mid + 1, r, target_idx=target_idx, root=BTUtil.right_idx(root))
+        self._t[root] = self.key(self._t[BTUtil.left_idx(root)],
+                                 self._t[BTUtil.right_idx(root)])
 
 
 class SegmentTreeWithNode:
+    """线段树 (节点存储).
+
+    和 `SegmentTree` 区别比较大的地方才有注释.
+    """
 
     def __init__(self, array: list, key: callable):
         self._a = array
@@ -133,59 +136,45 @@ class SegmentTreeWithNode:
     @classmethod
     def from_iterable(cls, iterable: Iterable, key: callable
                       ) -> 'SegmentTreeWithNode':
-        """根据 `iterable` 创建线段树, `key` 是融合函数."""
         array = list(iterable)
         segement = cls(array, key=key)
         segement.root = segement._build(0, len(segement) - 1)
         return segement
 
     def _build(self, l: int, r: int) -> Optional[BTNode]:
-        """以 `node` 为根构建线段树, 根代表的区间为 [l...r]."""
-        # 无效区间
+        """构建一棵区间为 [l...r] (根代表的区间) 的线段树, 返回构建完的树."""
         if l > r:
             return
-        # 区间无法再分割了
         if l == r:
             return BTNode(self._a[l])
 
-        # 区间分为 [l...mid], [mid+1...r]
         mid = (l + r) // 2
-
-        # 后序遍历, 左节点代表左区间, 右节点代表右区间
         node_l = self._build(l, mid)
         node_r = self._build(mid + 1, r)
         return BTNode(self.key(node_l.val, node_r.val), node_l, node_r)
 
     def _query(self, l, r, root: BTNode, query_l, query_r) -> Any:
-        """在以 `root` 为根的线段树中查询 [query_l...query_r], 根代表的区间为 [l...r]."""
         if l == query_l and r == query_r:
             return root.val
 
-        # 区间分为 [l...mid], [mid+1...r]
         mid = (l + r) // 2
-
-        # 如果查询区间是左或右区间的子集, 那么直接去左或右查
         if query_r <= mid:
             return self._query(l, mid, root=root.left,
                                query_l=query_l, query_r=query_r)
         elif query_l >= mid + 1:
             return self._query(mid + 1, r, root=root.right,
                                query_l=query_l, query_r=query_r)
-        # 查询区间横跨两个区间, 那么把查询区间分为 [query_l...mid], [mid+1...query_r]
+
         vl = self._query(l, mid, root=root.left, query_l=query_l, query_r=mid)
         vr = self._query(mid + 1, r, root=root.right, query_l=mid + 1, query_r=query_r)
         return self.key(vl, vr)
 
     def _update(self, l: int, r: int, target_idx: int, root: BTNode):
-        """在以 `root` 为根的线段树中, 更新包含 `target_idx` 的区间, 根代表的区间为 [l...r]."""
         if l == r == target_idx:
             root.val = self._a[l]
             return
 
-        # 区间分为 [l...mid], [mid+1...r]
         mid = (l + r) // 2
-
-        # `target_idx` 只可能在左区间或右区间
         if target_idx <= mid:
             self._update(l, mid, target_idx=target_idx, root=root.left)
         else:
