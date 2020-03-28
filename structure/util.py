f15078d3cc13f2ea71c025491335403ec5221282
dyq666 <694526892@qq.com>
2020-03-28 13:52:21 +0800

    opt: modify the doc and the typing of utils

--- structure/util.py
+++ structure/util.py
@@ -7,15 +7,10 @@ from typing import Any, Sized
 
 
 def check_index(offset: int = 0) -> callable:
-    """在执行数据结构某个方法前检查索引是否有效.
-
-    方法必须满足第一个参数是 `self`, 第二是索引, 同时类必须实现 `__len__`.
-
-    索引的检查范围是 [0, len + offset).
-    """
+    """检查索引是否在范围 [0, len + offset) 中."""
     def deco(f: callable) -> callable:
         @wraps(f)
-        def wrapper(self: Any, index: int, *args, **kwargs) -> Any:
+        def wrapper(self: Sized, index: int, *args: tuple, **kwargs: dict) -> Any:
             if not (0 <= index < len(self) + offset):
                 raise IndexError
             return f(self, index, *args, **kwargs)
@@ -26,9 +21,9 @@ def check_index(offset: int = 0) -> callable:
 
 
 def not_empty(f: callable) -> callable:
-    """在执行数据结构某个方法前检查数据结构是否为空."""
+    """检查实例是否为空."""
     @wraps(f)
-    def wrapper(self: Sized, *args, **kwargs) -> Any:
+    def wrapper(self: Sized, *args: tuple, **kwargs: dict) -> Any:
         if len(self) == 0:
             raise IndexError
         return f(self, *args, **kwargs)
