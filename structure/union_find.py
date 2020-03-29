96167b0e6319dba25d82338c8362ea4a2ce50de3
dyq666 <694526892@qq.com>
2020-03-29 10:42:18 +0800

    feature: use size to lower tree size

--- structure/union_find.py
+++ structure/union_find.py
@@ -61,10 +61,15 @@ class UnionFindV2:
     答: 索引代表每个实例的编号, 具体实例和编号的映射需要在并查集外部维护.
        值代表元素父亲的索引. 当索引和值相同时, 元素为根节点. 如果两个
        元素的根节点相同, 那么两个元素在一个集合中.
+
+    `self._size` 的索引和值各代表什么 ?
+
+    答: 索引和 `_parent` 中的索引意义相同. 值代表以当前索引为根的树节点个数.
     """
 
-    def __init__(self, parent: List[int]):
+    def __init__(self, parent: List[int], size: List[int]):
         self._parent = parent
+        self._size = size
 
     def __len__(self) -> int:
         return len(self._parent)
@@ -76,10 +81,16 @@ class UnionFindV2:
     def union(self, idx1: int, idx2: int):
         """合并两个元素所在的集合.
 
-        合并时必须将一个根节点指向另一个根节点, 这样可以最大程度的减小树的高度.
-        虽然让 idx1 的根节点指向 idx2, 也可以完成合并操作, 但会增加树的高度.
+        合并时将节点少的树合并到节点多的树中, 这样树的高度比反过来更低.
         """
-        self._parent[self._root(idx1)] = self._root(idx2)
+        root1, root2 = self._root(idx1), self._root(idx2)
+        size1, size2 = self._size[root1], self._size[root2]
+        if size1 < size2:
+            self._parent[root1] = root2
+            self._size[root2] += size1
+        else:
+            self._parent[root2] = root1
+            self._size[root1] += size2
 
     @check_index()
     def _root(self, index: int) -> int:
@@ -91,5 +102,5 @@ class UnionFindV2:
 
     @classmethod
     def generate(cls, size: int) -> 'UnionFindV2':
-        # 初始时, 每个元素都是根节点.
-        return UnionFindV2(list(range(size)))
+        # 初始时, 每个元素都是根节点 (值和索引相同), 每棵树的节点个数都为 1.
+        return UnionFindV2(list(range(size)), [1 for _ in range(size)])
