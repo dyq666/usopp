7ccab8843569b898b17453a3d71326150a26d3c8
dyq666 <694526892@qq.com>
2020-03-30 15:51:58 +0800

    opt: restructure MaxHeap

--- structure/heap.py
+++ structure/heap.py
@@ -27,9 +27,12 @@ class MaxHeap:
 
     @not_empty
     def pop(self) -> Any:
-        """删除元素.
+        """弹出最大值.
 
-        头和尾互换, 换完之后, 尾弹出, 头下沉.
+        是如何想到这种方式 ?
+
+        答: 由于底层是一个 list, list 从末尾弹出元素是 O(1) 的, 因此先将
+           最大值和末尾元素互换, 让最大值从末尾弹出, 再想办法让 list 维持最大堆的特性.
         """
         self._a[0], self._a[-1] = self._a[-1], self._a[0]
         max_ = self._a.pop()
@@ -39,22 +42,30 @@ class MaxHeap:
     def push(self, value: Any):
         """添加元素.
 
-        先将元素放到末尾, 再升上去.
+        是如何想到这种方式 ?
+
+        答: 由于底层是一个 list, list 从末尾添加元素是 O(1) 的, 因此先将
+           元素放到末尾, 再想办法让 list 维持最大堆的特性.
         """
         self._a.append(value)
         self._sift_up(len(self) - 1)
 
     def pushpop(self, value: Any) -> Any:
-        """先 `push` 一个元素, 再 `pop` 首元素."""
+        """等于先 `push` 一个元素, 再 `pop` 首元素.
+
+        和 `replace` 的区别是 ?
+
+        答: 当 `push` 的元素大于堆中最大值时, `pop` 的也会是这个元素,
+           因此堆不需要改变.
+        """
         if len(self) == 0 or value >= self._a[0]:
             return value
 
-        # 当 `value < max_` 时, 和先 `pop` 后 `push` 就是一样的
         return self.replace(value)
 
     @not_empty
     def replace(self, value: Any) -> Any:
-        """先 `pop` 首元素, 再 `push` 一个元素.
+        """等价于先 `pop` 首元素, 再 `push` 一个元素.
 
         :) 实际上这个方法可以叫做 `poppush`!
         """
@@ -65,30 +76,28 @@ class MaxHeap:
 
     def _sift_up(self, index: int):
         """上浮元素."""
-        d = self._a
-        # 如果没上浮到头并且比父亲大, 则继续上浮
-        while index > 0 and d[index] > d[BTUtil.parent_idx(index)]:
+        # 没上浮到根 (0 为根), 且比父大, 则上浮.
+        while index > 0 and self._a[index] > self._a[BTUtil.parent_idx(index)]:
             parent = BTUtil.parent_idx(index)
-            d[index], d[parent] = d[parent], d[index]
+            self._a[index], self._a[parent] = self._a[parent], self._a[index]
             index = parent
 
     def _sift_down(self, index: int):
         """下沉元素."""
-        d = self._a
         l, r = BTUtil.left_idx(index), BTUtil.right_idx(index)
         # 当左 >= len 时, 等价于 `index` 没有左, 由于右 = 左 + 1, 因而也没有右,
         # 所以此时 `index` 是叶子, 叶子无法继续下沉.
         while l < len(self):
             # 如果有右节点, 则从左右中选择一个大的.
             if r < len(self):
-                child = l if d[l] > d[r] else r
+                child = l if self._a[l] > self._a[r] else r
             else:
                 child = l
 
             # 父比孩子中最大的还大, 不需要下沉了.
-            if d[index] >= d[child]:
+            if self._a[index] >= self._a[child]:
                 break
-            d[child], d[index] = d[index], d[child]
+            self._a[child], self._a[index] = self._a[index], self._a[child]
             index = child
             l, r = BTUtil.left_idx(index), BTUtil.right_idx(index)
 
@@ -96,17 +105,15 @@ class MaxHeap:
     def heapify(cls, array: Iterable) -> 'MaxHeap':
         """使任意排序的数组转换成堆.
 
-        从最后一个非叶子节点到第一个节点逐个下沉 (实际上从最后一个
-        节点开始下沉也是可以的, 只不过叶子节点下沉等于什么都不干).
-
-        heapify 的时间复杂度是 O(N) !
+        时间复杂度是 O(N) (具体原因请参考网上资料) !
         """
         array = list(array)
+        # 长度 < 2 的数组已经是最大堆了
         if len(array) < 2:
             return cls(array)
 
-        last = BTUtil.parent_idx(len(array) - 1)
         heap = cls(array)
-        for i in range(last, -1, -1):
+        # 从最后一个非叶子到根逐个下沉
+        for i in range(BTUtil.parent_idx(len(array) - 1), -1, -1):
             heap._sift_down(i)
         return heap
--- test/test_heap.py
+++ test/test_heap.py
@@ -12,110 +12,75 @@ def test_push():
     ```
     """
     heap = MaxHeap()
+    steps = {
+        2: [2],
+        3: [3, 2],
+        1: [3, 2, 1],
+        5: [5, 3, 1, 2],
+        0: [5, 3, 1, 2, 0],
+        4: [5, 3, 4, 2, 0, 1],
+        6: [6, 3, 5, 2, 0, 1, 4],
+    }
 
-    heap.push(2)
-    assert len(heap) == 1
-    assert list(heap) == [2]
-
-    heap.push(3)
-    assert len(heap) == 2
-    assert list(heap) == [3, 2]
-
-    heap.push(1)
-    assert len(heap) == 3
-    assert list(heap) == [3, 2, 1]
-
-    heap.push(5)
-    assert len(heap) == 4
-    assert list(heap) == [5, 3, 1, 2]
-
-    heap.push(0)
-    assert len(heap) == 5
-    assert list(heap) == [5, 3, 1, 2, 0]
-
-    heap.push(4)
-    assert len(heap) == 6
-    assert list(heap) == [5, 3, 4, 2, 0, 1]
-
-    heap.push(6)
-    assert len(heap) == 7
-    assert list(heap) == [6, 3, 5, 2, 0, 1, 4]
+    for val, array in steps.items():
+        heap.push(val)
+        assert list(heap) == array
 
 
 def test_pop():
-    """按以下倒序删除元素.
+    """按以下顺序删除元素.
     ```
        6        5       4       3       2      1    0
      3   5    3   4   3   1   2   1   0  1   0
     2 0 1 4  2 0 1   2 0     0
     ```
     """
-    heap = MaxHeap()
-    for i in [6, 3, 5, 2, 0, 1, 4]:
-        heap.push(i)
+    heap = MaxHeap([6, 3, 5, 2, 0, 1, 4])
+    steps = {
+        6: [5, 3, 4, 2, 0, 1],
+        5: [4, 3, 1, 2, 0],
+        4: [3, 2, 1, 0],
+        3: [2, 0, 1],
+        2: [1, 0],
+        1: [0],
+        0: [],
+    }
 
-    assert 6 == heap.pop()
-    assert len(heap) == 6
-    assert list(heap) == [5, 3, 4, 2, 0, 1]
-
-    assert 5 == heap.pop()
-    assert len(heap) == 5
-    assert list(heap) == [4, 3, 1, 2, 0]
-
-    assert 4 == heap.pop()
-    assert len(heap) == 4
-    assert list(heap) == [3, 2, 1, 0]
-
-    assert 3 == heap.pop()
-    assert len(heap) == 3
-    assert list(heap) == [2, 0, 1]
-
-    assert 2 == heap.pop()
-    assert len(heap) == 2
-    assert list(heap) == [1, 0]
-
-    assert 1 == heap.pop()
-    assert len(heap) == 1
-    assert list(heap) == [0]
-
-    assert 0 == heap.pop()
-    assert len(heap) == 0
-    assert list(heap) == []
+    for val, array in steps.items():
+        assert val == heap.pop()
+        assert list(heap) == array
 
     with pytest.raises(IndexError):
         heap.pop()
 
 
 def test_pushpop_and_replace():
-    # 空
+    # 空堆时, pushpop 行, replace 不行
     with pytest.raises(IndexError):
         MaxHeap().replace(1)
     assert 1 == MaxHeap().pushpop(1)
 
-    # 比较二者放入一个比堆最大值还大的元素
-    heap = MaxHeap()
-    for i in range(8, 5, -1):
-        heap.push(i)
+    # 放入一个比堆最大值还大的元素, pushpop 后, 堆不变, replace 后, 堆变.
+    heap = MaxHeap([8, 7, 6])
     assert 9 == heap.pushpop(9)
     assert list(heap) == [8, 7, 6]
     assert 8 == heap.replace(9)
     assert list(heap) == [9, 7, 6]
 
-    # 比较二者放入一个小于堆最大值的元素
-    heap = MaxHeap()
-    for i in range(8, 5, -1):
-        heap.push(i)
+    # # 放入一个比堆最大值小的元素, pushpop 后, 堆变, replace 后, 堆也变.
+    heap = MaxHeap([8, 7, 6])
     assert 8 == heap.replace(4)
     assert list(heap) == [7, 4, 6]
-    heap = MaxHeap()
-    for i in range(8, 5, -1):
-        heap.push(i)
+    heap = MaxHeap([8, 7, 6])
     assert 8 == heap.pushpop(4)
     assert list(heap) == [7, 4, 6]
 
 
 def test_heapify():
+    # 长度 < 2 的数组已经是最大堆了
     assert list(MaxHeap.heapify([])) == []
     assert list(MaxHeap.heapify([2])) == [2]
-    assert list(MaxHeap.heapify([2, 3])) == [3, 2]
+
+    # 奇数 / 偶数
+    assert list(MaxHeap.heapify([2, 3, 1, 4])) == [4, 3, 1, 2]
     assert list(MaxHeap.heapify([0, 4, 3, 2, 1])) == [4, 2, 3, 0, 1]
