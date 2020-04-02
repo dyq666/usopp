96184ecfb20d5ecfc6b07c83bdcff76373d7e9d5
dyq666 <694526892@qq.com>
2020-04-02 12:46:51 +0800

    feature: add remove in AVL

--- structure/avl.py
+++ structure/avl.py
@@ -2,9 +2,10 @@ __all__ = (
     'AVL',
 )
 
-from typing import Any, Iterable, Iterator, Optional
+from typing import Any, Iterable, Iterator, Optional, Tuple
 
 from .tree import BTNode, BTUtil
+from .util import not_empty
 
 
 class AVL:
@@ -24,6 +25,11 @@ class AVL:
     def add(self, key: Any, value: Any = 0):
         self._root = self._add(self._root, key, value)
 
+    @not_empty
+    def remove(self, key: Any):
+        """从树中删除键为 `key` 的节点."""
+        self._root = self._remove(self._root, key)
+
     def _add(self, root: Optional[BTNode], key: Any, value: Any = 0):
         """在以 `root` 为根的树中添加节点, 返回添加完成后的树."""
         if root is None:
@@ -41,79 +47,27 @@ class AVL:
         self._flush_height(root)
         return self._balance(root)
 
-    def _balance(self, node: BTNode) -> BTNode:
-        """
-        问: 为什么 AVL 树分成四种需要平衡的情况 ?
+    def _remove(self, root: BTNode, key: Any) -> Optional[BTNode]:
+        """从以 `root` 为根的树中删除键为 `key` 的节点, 返回删除后的树."""
+        # `key` 不在树中
+        if root is None:
+            raise ValueError
+        # 找到了删除节点
+        if key == root.key:
+            if root.right and root.left:
+                root.key, root.val, root.left = self._pop_max_from(root.left)
+                res = root
+            else:
+                res = root.right or root.left or None
+            self._size -= 1
+            return res
 
-        答: 如果一个节点由平衡转为不平衡, 那么一定是左或右子树的某个地方添加了
-           一个元素, 此时分成两种需要平衡的情况, 但没有办法在已知的数据下, 将这
-           两种情况恢复平衡性, 因此需要继续细分. 左子树添加元素分为左左或左右添
-           加了元素, 右子树添加元素分为右左或右右添加了元素, 在这四种情况下都可
-           以恢复平衡性, 因此不需要继续细分.
-
-        TODO 一个遗留的疑问, 在 LL 时, 我并没有找到根平衡因子为 2, 但左孩子平衡
-             因子为 0 的情况. 同理, 在 RR 时, 我也没有找到根平衡因子为 2, 但右
-             孩子平衡因子为 0 的情况. LR, RR 也一样.
-
-        LL: 初始情况, 根的平衡因子为 1, 左孩子的平衡因子为 0. 添加元素之后,
-            根的平衡因子为 2, 左孩子的平衡因子为 1. 右旋后 (根挂到左孩子的右边,
-            左孩子的右边挂到根的左边), 由于根节点下降了两层, 高度从 h+3 -> h+1.
-            其他节点高度不变, 使得根和右孩子的平衡因子都变为 0. 而且依然满足二叉
-            搜索树的特性, 因此平衡成功.
-        ```
-              A(h+2)                 A(h+3)                 B(h+2)
-          B(h+1)  C(h)    ->      B(h+2)  C(h)    ->   D(h+1)  A(h+1)
-        D(h) E(h)              D(h+1) E(h)                  E(h)  C(h)
-        ```
-
-         RR: 初始情况, 根的平衡因子为 -1, 右孩子的平衡因子为 0. 添加元素之后,
-             根的平衡因子为 -2, 右孩子的平衡因子为 -1. 左旋后 (根挂到右孩子的左边,
-             右孩子的左边挂到根的右边), 由于根节点下降了两层, 高度从 h+3 -> h+1.
-             其他节点高度不变, 使得根和右孩子的平衡因子都变为 0. 而且依然满足二叉
-             搜索树的特性, 因此平衡成功.
-        ```
-              A(h+2)                A(h+3)                 C(h+2)
-          B(h)  C(h+1)    ->    B(h)  C(h+2)    ->      A(h+1)  E(h+1)
-              D(h) E(h)             D(h) E(h+1)      B(h) D(h)
-        ```
-
-        LR: 初始情况, 根的平衡因子为 1, 左孩子的平衡因子为 0. 添加元素之后,
-            根的平衡因子为 2, 左孩子的平衡因子为 -1. 先让左孩子左旋转, 然后
-            变成了 LL 的情况, 按照 LL 进行处理即可.
-        ```
-                A(h+2)               A(h+3)              A(h+3)
-            B(h+1)  C(h)    ->    B(h+2)  C(h)   ->   E(h+2)  C(h)
-         D(h)  E(h)            D(h) E(h+1)          B(h+1)
-                                                  D(h)
-        ```
-
-        RL: 初始情况, 根的平衡因子为 -1, 右孩子的平衡因子为 0, 添加元素之后,
-            根的平衡因子为 -2, 右孩子的平衡因子为 -1. 先让右孩子右旋转, 然后
-            变成了 RR 的情况, 按照 RR 进行处理节课.
-        ```
-                A(h+2)               A(h+3)              A(h+3)
-            B(h)  C(h+1)    ->    B(h)  C(h+2)   ->   B(h)  D(h+2)
-               D(h)  E(h)              D(h+1) E(h)            C(h+1)
-                                                                 E(h)
-        ```
-        """
-        balance = self._get_balance(node)
-        # LL
-        if balance > 1 and self._get_balance(node.left) >= 0:
-            return self._right_rotate(node)
-        # RR
-        if balance < -1 and self._get_balance(node.right) <= 0:
-            return self._left_rotate(node)
-        # LR
-        if balance > 1 and self._get_balance(node.left) < 0:
-            node.left = self._left_rotate(node.left)
-            return self._right_rotate(node)
-        # RL
-        if balance < -1 and self._get_balance(node.right) > 0:
-            node.right = self._right_rotate(node.right)
-            return self._left_rotate(node)
-
-        return node
+        if key < root.key:
+            root.left = self._remove(root.left, key)
+        else:  # key > root.key
+            root.right = self._remove(root.right, key)
+        self._flush_height(root)
+        return self._balance(root)
 
     @classmethod
     def from_iterable(cls, iterable: Iterable) -> 'AVL':
@@ -169,3 +123,90 @@ class AVL:
         if node is None:
             return 0
         return AVL._get_height(node.left) - AVL._get_height(node.right)
+
+    @staticmethod
+    def _pop_max_from(root: BTNode) -> Tuple[Any, Any, Optional[BTNode]]:
+        """删除以 `root` 为根的树中最大节点, 返回最大值和删除之后的树."""
+        # 找到最大值了
+        if root.right is None:
+            return root.key, root.val, root.left
+
+        k, v, n = AVL._pop_max_from(root.right)
+        root.right = n
+        AVL._flush_height(root)
+        return k, v, AVL._balance(root)
+
+    @staticmethod
+    def _balance(node: BTNode) -> BTNode:
+        """
+        问: 为什么 AVL 树分成四种需要平衡的情况 ?
+
+        答: 如果一个节点由平衡转为不平衡, 那么一定是左或右子树的某个地方添加了
+           一个元素, 此时分成两种需要平衡的情况, 但没有办法在已知的数据下, 将这
+           两种情况恢复平衡性, 因此需要继续细分. 左子树添加元素分为左左或左右添
+           加了元素, 右子树添加元素分为右左或右右添加了元素, 在这四种情况下都可
+           以恢复平衡性, 因此不需要继续细分.
+
+        TODO 一个遗留的疑问, 在 LL 时, 我并没有找到根平衡因子为 2, 但左孩子平衡
+             因子为 0 的情况. 同理, 在 RR 时, 我也没有找到根平衡因子为 2, 但右
+             孩子平衡因子为 0 的情况. LR, RR 也一样.
+
+        LL: 初始情况, 根的平衡因子为 1, 左孩子的平衡因子为 0. 添加元素之后,
+            根的平衡因子为 2, 左孩子的平衡因子为 1. 右旋后 (根挂到左孩子的右边,
+            左孩子的右边挂到根的左边), 由于根节点下降了两层, 高度从 h+3 -> h+1.
+            其他节点高度不变, 使得根和右孩子的平衡因子都变为 0. 而且依然满足二叉
+            搜索树的特性, 因此平衡成功.
+        ```
+              A(h+2)                 A(h+3)                 B(h+2)
+          B(h+1)  C(h)    ->      B(h+2)  C(h)    ->   D(h+1)  A(h+1)
+        D(h) E(h)              D(h+1) E(h)                  E(h)  C(h)
+        ```
+
+         RR: 初始情况, 根的平衡因子为 -1, 右孩子的平衡因子为 0. 添加元素之后,
+             根的平衡因子为 -2, 右孩子的平衡因子为 -1. 左旋后 (根挂到右孩子的左边,
+             右孩子的左边挂到根的右边), 由于根节点下降了两层, 高度从 h+3 -> h+1.
+             其他节点高度不变, 使得根和右孩子的平衡因子都变为 0. 而且依然满足二叉
+             搜索树的特性, 因此平衡成功.
+        ```
+              A(h+2)                A(h+3)                 C(h+2)
+          B(h)  C(h+1)    ->    B(h)  C(h+2)    ->      A(h+1)  E(h+1)
+              D(h) E(h)             D(h) E(h+1)      B(h) D(h)
+        ```
+
+        LR: 初始情况, 根的平衡因子为 1, 左孩子的平衡因子为 0. 添加元素之后,
+            根的平衡因子为 2, 左孩子的平衡因子为 -1. 先让左孩子左旋转, 然后
+            变成了 LL 的情况, 按照 LL 进行处理即可.
+        ```
+                A(h+2)               A(h+3)              A(h+3)
+            B(h+1)  C(h)    ->    B(h+2)  C(h)   ->   E(h+2)  C(h)
+         D(h)  E(h)            D(h) E(h+1)          B(h+1)
+                                                  D(h)
+        ```
+
+        RL: 初始情况, 根的平衡因子为 -1, 右孩子的平衡因子为 0, 添加元素之后,
+            根的平衡因子为 -2, 右孩子的平衡因子为 -1. 先让右孩子右旋转, 然后
+            变成了 RR 的情况, 按照 RR 进行处理节课.
+        ```
+                A(h+2)               A(h+3)              A(h+3)
+            B(h)  C(h+1)    ->    B(h)  C(h+2)   ->   B(h)  D(h+2)
+               D(h)  E(h)              D(h+1) E(h)            C(h+1)
+                                                                 E(h)
+        ```
+        """
+        balance = AVL._get_balance(node)
+        # LL
+        if balance > 1 and AVL._get_balance(node.left) >= 0:
+            return AVL._right_rotate(node)
+        # RR
+        if balance < -1 and AVL._get_balance(node.right) <= 0:
+            return AVL._left_rotate(node)
+        # LR
+        if balance > 1 and AVL._get_balance(node.left) < 0:
+            node.left = AVL._left_rotate(node.left)
+            return AVL._right_rotate(node)
+        # RL
+        if balance < -1 and AVL._get_balance(node.right) > 0:
+            node.right = AVL._right_rotate(node.right)
+            return AVL._left_rotate(node)
+
+        return node
--- test/test_avl.py
+++ test/test_avl.py
@@ -33,3 +33,39 @@ def test_add():
         assert list(avl) == res
         assert BST.is_bst(avl._root)
         assert AVL.is_avl(avl._root)
+
+
+def test_remove():
+    """
+    一些简单树的四种情况, ** 代表删除的元素, LL, LR, RR, RL.
+    ```
+      3        3         3         3
+     2 *4*    1 *4*   *1*  4     *1* 5
+    1          2             5      4
+
+    一些复杂树的四种情况, ** 代表删除的元素, LL, LR, RR, RL.
+    ```
+          4            4            4           3
+        1   5        1   5        2   6       2  6
+      0  2   *6*   0  3   *6*   *1*  5 7    *1* 5 7
+    -1空          空空2空            空空空8      4空空空
+    ```
+
+    TODO 一些删除侧复杂的情况, 主要是测试 `AVL._pop_max_from` 中的 balance.
+    """
+    items = (
+        ([3, 2, 4, 1], 4, [2, 1, 3]),
+        ([3, 1, 4, 2], 4, [2, 1, 3]),
+        ([3, 1, 4, 5], 1, [4, 3, 5]),
+        ([3, 1, 5, 4], 1, [4, 3, 5]),
+        ([4, 1, 5, 0, 2, 6, -1], 6, [1, 0, 4, -1, None, 2, 5]),
+        ([4, 1, 5, 0, 3, 6, 2], 6, [3, 1, 4, 0, 2, None, 5]),
+        ([4, 2, 6, 1, 5, 7, 8], 1, [6, 4, 7, 2, 5, None, 8]),
+        ([3, 2, 6, 1, 5, 7, 4], 1, [5, 3, 6, 2, 4, None, 7]),
+    )
+    for array, delete, res in items:
+        avl = AVL.from_iterable(array)
+        avl.remove(delete)
+        assert list(avl) == res
+        assert BST.is_bst(avl._root)
+        assert AVL.is_avl(avl._root)
